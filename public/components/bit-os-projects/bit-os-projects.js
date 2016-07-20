00515670bc3721faf5c414e3784dd0ba3a382c41
Garrett Reed <garrett@garrettreed.co>
2016-07-20 09:48:29 -0500

    Add os project vm

--- public/components/bit-os-projects/bit-os-projects.js
+++ public/components/bit-os-projects/bit-os-projects.js
@@ -4,14 +4,15 @@ import './bit-os-projects.less';
 import template from './bit-os-projects.stache';
 import OSProjectModel from '../../models/os-project';
 import ContributionMonthModel from '../../models/contribution-month';
-import ContributionMonthModelFixture from '../../models/fixtures/contribution-month';
-
-// console.log(ContributionMonthModelFixture.findAll({url: "/api/contribution_months", dataType: "json"}, function() {
-//   console.log(arguments);
-// }));
 
 export const ViewModel = DefineMap.extend({
-    contributionMonth: ContributionMonthModel
+    contributionMonth: {
+        Value: ContributionMonthModel
+    },
+    logIt: function(it) {
+        console.log('logging');
+        console.log(it);
+    }
 });
 
 
