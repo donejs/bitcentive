2bc231d23bbee64b8d24e8ceddc34f8819a6a684
Justin Meyer <justinbmeyer@gmail.com>
2016-07-20 16:35:13 -0500

    makes it possible for sub-types to have unified instances

--- public/models/os-project.js
+++ public/models/os-project.js
@@ -1,6 +1,27 @@
+import set from "can-set";
 import DefineMap from "can-define/map/";
+import DefineList from "can-define/list/";
+import superMap from "can-connect/can/super-map/";
 
-export default DefineMap.extend({
+
+var OSProject =  DefineMap.extend("OSProject",{
   _id: "string",
   name: "string"
-})
+});
+
+
+
+var osProjectAlgebra = new set.Algebra(
+    set.comparators.id("_id")
+);
+
+OSProject.connection = superMap({
+  Map: OSProject,
+  List: OSProject.List,
+  url: "/api/client_projects",
+  name: "contributionMonth",
+  algebra: osProjectAlgebra
+});
+
+
+export default OSProject;
