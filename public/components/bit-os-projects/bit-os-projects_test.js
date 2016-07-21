6245e07b1d8798af2c28c9e5b28306ec47ea218b
Justin Meyer <justinbmeyer@gmail.com>
2016-07-21 10:22:41 -0500

    minor fixes to os-projects after review

--- public/components/bit-os-projects/bit-os-projects_test.js
+++ public/components/bit-os-projects/bit-os-projects_test.js
@@ -4,7 +4,26 @@ import { ViewModel } from './bit-os-projects';
 // ViewModel unit tests
 QUnit.module('bitcentive/components/bit-os-projects');
 
-QUnit.test('Has adding property', function(){
+/*QUnit.test('Has adding property', function(){
   var vm = new ViewModel();
-  QUnit.equal(vm.adding, false);
-});
+
+  vm.contributionMonth = new ContributionMonth();
+  this.selectedOSProjectId = "__new__";
+  vm.newOSProjectName = "something";
+
+  fixture("POST /api/os_projects", function(req){
+    QUnit.equal(req.data.name , "something");
+    res({_id: Math.random()})
+  });
+
+  fixture("POST /api/contribution_month", function(req){
+    QUnit.deepEqual(req.data.monthlyOSProjects[1] , {});
+    res({_id: Math.random()})
+  });
+  vm.addNewProject().then(function(){
+    QUnit.start();
+  }, function(err){
+    QUnit.ok(false, "error "+err)
+    QUnit.start();
+  });
+});*/
