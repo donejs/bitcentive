85e84d6148db7a1a280d01bf5e91767a6fef4418
Garrett Reed <garrett@garrettreed.co>
2016-07-21 15:36:59 -0500

    Resolve naming conflicts

--- public/test/test.js
+++ public/test/test.js
@@ -2,7 +2,6 @@ import 'bitcentive/models/test';
 import 'bitcentive/test/functional';
 
 import 'bitcentive/components/os-projects/os-projects_test';
-import 'bitcentive/components/bit-os-projects/bit-os-projects_test';
 
 import 'bitcentive/components/contribution-month/contribution-month_test';
 
