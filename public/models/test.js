1d47ad08aecb128b476dabd6e30a2e232ae166d0
John Gardner <john@ardea.io>
2016-07-21 10:39:36 -0500

    Initial

--- public/models/test.js
+++ public/models/test.js
@@ -1,61 +1,65 @@
 import './fixtures/';
 
 import QUnit from "steal-qunit";
-import ContributionMonth from "./contribution-month";
-import ClientProject from "./client-project";
-import OSProject from "./os-project";
+//import ContributionMonth from "./contribution-month";
+//import ClientProject from "./client-project";
+//import OSProject from "./os-project";
 
 QUnit.module("models");
 
-QUnit.asyncTest("getList of ContributionMonth", function() {
-	ContributionMonth.getList({}).then(function(contributionMonths) {
-
-	  console.log(contributionMonths);
-		QUnit.ok(contributionMonths[0].monthlyClientProjects[0].clientProject instanceof ClientProject);
-		var first = contributionMonths[0].monthlyOSProjects[0].osProject,
-			second = contributionMonths[0].monthlyClientProjects[0].monthlyClientProjectsOsProjects[0].osProject;
-
-    console.log(first);
-    console.log(second);
-
-		QUnit.ok(first === second);
-		QUnit.start();
-	}, function(err) {
-		debugger;
-	});
+QUnit.test("Simple test", function() {
+  QUnit.ok(true);
 });
 
-QUnit.test("make type convert able to accept instances (#23)", function() {
-	var osProject = new OSProject({
-			_id: "somethingCrazey",
-			name: "CanJS"
-	});
-
-	var clientProject = new ClientProject({
-			_id: "asl;dfal;sfj ;lakwj",
-			name: "HualHound"
-	});
-
-	var contributionMonth = new ContributionMonth({
-			_id: "aslkfalsjklas",
-			date: 124234211310000,
-			monthlyOSProjects: [{
-					significance: 80,
-					commissioned: true,
-					osProjectId: osProject._id,
-					osProject: osProject
-			}],
-			monthlyClientProjects: [{
-					monthlyClientProjectsOsProjects: [{
-							osProjectId: osProject._id,
-							osProject: osProject
-					}],
-					hours: 100,
-					clientProjectId: clientProject._id,
-					clientProject: clientProject
-			}]
-	});
-
-	QUnit.equal(contributionMonth.monthlyOSProjects[0].osProject.name , "CanJS")
-
-});
+// QUnit.asyncTest("getList of ContributionMonth", function() {
+// 	ContributionMonth.getList({}).then(function(contributionMonths) {
+//
+// 	  console.log(contributionMonths);
+// 		QUnit.ok(contributionMonths[0].monthlyClientProjects[0].clientProject instanceof ClientProject);
+// 		var first = contributionMonths[0].monthlyOSProjects[0].osProject,
+// 			second = contributionMonths[0].monthlyClientProjects[0].monthlyClientProjectsOsProjects[0].osProject;
+//
+//     console.log(first);
+//     console.log(second);
+//
+// 		QUnit.ok(first === second);
+// 		QUnit.start();
+// 	}, function(err) {
+// 		debugger;
+// 	});
+// });
+//
+// QUnit.test("make type convert able to accept instances (#23)", function() {
+// 	var osProject = new OSProject({
+// 			_id: "somethingCrazey",
+// 			name: "CanJS"
+// 	});
+//
+// 	var clientProject = new ClientProject({
+// 			_id: "asl;dfal;sfj ;lakwj",
+// 			name: "HualHound"
+// 	});
+//
+// 	var contributionMonth = new ContributionMonth({
+// 			_id: "aslkfalsjklas",
+// 			date: 124234211310000,
+// 			monthlyOSProjects: [{
+// 					significance: 80,
+// 					commissioned: true,
+// 					osProjectId: osProject._id,
+// 					osProject: osProject
+// 			}],
+// 			monthlyClientProjects: [{
+// 					monthlyClientProjectsOsProjects: [{
+// 							osProjectId: osProject._id,
+// 							osProject: osProject
+// 					}],
+// 					hours: 100,
+// 					clientProjectId: clientProject._id,
+// 					clientProject: clientProject
+// 			}]
+// 	});
+//
+// 	QUnit.equal(contributionMonth.monthlyOSProjects[0].osProject.name , "CanJS")
+//
+// });
