2ac41d15b78166f48e08e7078f77b920504bd625
Kyle Gifford <kyle@kylegifford.com>
2016-08-25 16:02:54 -0500

    fix contributors lookup

--- public/models/contributor.js
+++ public/models/contributor.js
@@ -1,6 +1,7 @@
 import DefineMap from "can-define/map/";
 import DefineList from "can-define/list/";
 import superMap from "can-connect/can/super-map/";
+import feathers from "./feathers";
 
 var Contributor = DefineMap.extend("Contributor", {
   _id: "string",
@@ -20,7 +21,7 @@ Contributor.connection = superMap({
   idProp: "_id",
   Map: Contributor,
   List: Contributor.List,
-  url: "/api/contributors",
+  url: feathers.rest("/api/contributors"),
   name: "contributor",
   idProp: "_id"
 });
