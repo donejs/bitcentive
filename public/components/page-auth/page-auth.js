1e7a832413e5bac37cbcb19ff29ff2121e4ab638
Marshall Thompson <marshall@creativeideal.net>
2016-10-26 16:22:22 -0600

    Switch to using local auth component
    instead of auth-component.  We’re going to show the user how to build a login form.

--- public/components/page-auth/page-auth.js
+++ public/components/page-auth/page-auth.js
@@ -2,10 +2,16 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './page-auth.less!';
 import template from './page-auth.stache!';
+import User from 'bitcentive/models/user';
 
 export const ViewModel = DefineMap.extend({
   subpage: {}, // bound to the subpage in the AppState.
   session: '*',
+  email: 'string',
+  password: 'string',
+  setSubpage(tabName) {
+    this.subpage = tabName;
+  },
   isOAuthRoute: {
     get(){
       return this.subpage === 'success' || this.subpage === 'failure';
