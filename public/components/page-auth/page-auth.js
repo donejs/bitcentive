14e523fa0376325ca314ef6348767399cd681f82
Marshall Thompson <marshall@creativeideal.net>
2016-11-02 15:43:49 -0600

    Auth page is only a GitHub button, now

--- public/components/page-auth/page-auth.js
+++ public/components/page-auth/page-auth.js
@@ -5,19 +5,7 @@ import template from './page-auth.stache!';
 import openLoginPopup from 'authentication-popups';
 
 export const ViewModel = DefineMap.extend({
-  subpage: {}, // bound to the subpage in the AppState.
-  session: '*',
-  email: 'string',
-  password: 'string',
-  setSubpage (tabName) {
-    this.subpage = tabName;
-  },
-  openLoginPopup,
-  isOAuthRoute: {
-    get () {
-      return this.subpage === 'success' || this.subpage === 'failure';
-    }
-  }
+  openLoginPopup
 });
 
 export default Component.extend({
