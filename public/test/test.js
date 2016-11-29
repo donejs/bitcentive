72e5425405fed8ea0795dcfb7ebed5187ccf1311
Ilya Fadeev <fadeev.ilya@gmail.com>
2016-11-29 14:53:55 -0500

    added contributors test to main test suite

--- public/test/test.js
+++ public/test/test.js
@@ -5,6 +5,8 @@ import 'bitcentive/components/os-projects/os-projects_test';
 
 import 'bitcentive/components/contribution-month/contribution-month_test';
 
+import 'bitcentive/components/contributors/contributors_test';
+
 import 'bitcentive/components/select-contribution-month/select-contribution-month_test';
 
 import 'bitcentive/components/login/login_test';
