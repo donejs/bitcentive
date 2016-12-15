3914341bcad348f887d84a9567e88b7653a59602
Marshall Thompson <marshall@creativeideal.net>
2016-12-15 10:49:24 -0700

    Test high QUnit timeout

--- public/test/functional.js
+++ public/test/functional.js
@@ -3,6 +3,9 @@ import QUnit from 'steal-qunit';
 
 F.attach(QUnit);
 
+// Set QUnit timeout to 5 minutes.
+QUnit.config.testTimeout = 300000;
+
 QUnit.module('bitcentive functional smoke test', {
   beforeEach() {
     F.open('../development.html');
