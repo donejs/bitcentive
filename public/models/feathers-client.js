98514e04079d7e1749a19cee4b76310edc103f36
Marshall Thompson <marshall@creativeideal.net>
2016-12-14 23:09:20 -0700

    Switch back to steal-socket.io

--- public/models/feathers-client.js
+++ public/models/feathers-client.js
@@ -1,5 +1,5 @@
 import feathers from 'feathers/client';
-import io from 'socket.io-client/dist/socket.io';
+import io from 'steal-socket.io';
 import socketio from 'feathers-socketio/client';
 import auth from 'feathers-authentication-client';
 import hooks from 'feathers-hooks';
