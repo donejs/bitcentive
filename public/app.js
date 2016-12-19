ccda6d631d159e2cb3ff088fcc0025949f6c4cac
Marshall Thompson <marshall@creativeideal.net>
2016-12-19 16:56:59 -0700

    Set initial session in its own property.

--- public/app.js
+++ public/app.js
@@ -27,16 +27,28 @@ const AppViewModel = DefineMap.extend({
     serialize: false
   },
 
+  // Once https://github.com/canjs/can-define-stream/issues/15 is fixed, 
+  // we can move the initialSession into the session `value` function.
+  initialSession: {
+    value () {
+      Session.get().then(session => {
+        this.initialSession = session;
+      })
+      .catch(err => console.log(err));
+    }
+  },
+
   /**
    * Use Session.get() to see if there's a valid JWT. If one exists, 
    * a new Session will be created.
    */
   session: {
+    // value () {},
     stream (setStream) {
-      Session.get().catch(err => console.log(err));
       return canStream.toStream(Session, 'created')
         .merge(canStream.toStream(Session, 'destroyed'))
-        .map(event => event.type === 'created' ? event.args[0] : undefined);
+        .map(event => event.type !== 'destroyed' ? event.args[0] : undefined)
+        .merge(this.stream('.initialSession'));
     }
   },
 
