b536f5f5ab254ffa8b2d8768a90f1abec8dd0995
Alfredo Delgado <alfred@adelgado.org>
2017-01-12 10:39:57 -0500

    Refactor payouts so that the map of unique OS projects can also be used to display their names in the view.

--- public/components/payouts/payouts.js
+++ public/components/payouts/payouts.js
@@ -7,15 +7,34 @@ import moment from "moment";
 export const ViewModel = DefineMap.extend({
   // Passed properties
   contributionMonth: ContributionMonth,
+  /**
+   * @property {Moment} currentMonth
+   *
+   * The current contribution month.
+   */
+  get currentMonth() {
+    return this.contributionMonth && moment(this.contributionMonth.date);
+  },
   contributionMonthsPromise: {
-    value() {
-      return ContributionMonth.getList({});
-    }
+		value() {
+			return ContributionMonth.getList({});
+		}
   },
   contributionMonths: {
     get(initial, resolve){
       this.contributionMonthsPromise.then(resolve);
     }
+  },
+  /**
+   * @property {List} effectiveMonths
+   *
+   * A list of contribution months including the current month and before.
+   */
+  get effectiveMonths() {
+    return this.contributionMonths &&
+      this.contributionMonths.filter(contributionMonth => {
+        return this.currentMonth.isSameOrAfter(contributionMonth.date);
+      });
   },
   getOSProjectPayoutTotal(monthlyOSProject, contributor) {
     let total = 0;
@@ -57,6 +76,22 @@ export const ViewModel = DefineMap.extend({
     return total;
   },
   /**
+   * @property {Map} monthlyOSProjects
+   *
+   * A unique set of OS Projects from the current and previous months.
+   */
+  get monthlyOSProjects() {
+    let uniqueProjects = {};
+
+    this.effectiveMonths && this.effectiveMonths.map(contributionMonth => {
+      contributionMonth.monthlyOSProjects.map(monthlyOSProject => {
+        uniqueProjects[monthlyOSProject._id] = monthlyOSProject;
+      });
+    });
+
+    return uniqueProjects;
+  },
+  /**
    * @property {Map} payouts
    *
    * A map of OS Project payouts per Contributor based on the current month and
@@ -89,27 +124,19 @@ export const ViewModel = DefineMap.extend({
     let contributorProjectPayouts = {};
 
     if (this.contributionMonth && this.contributionMonths) {
-      let currentMonth = moment(this.contributionMonth.date);
-      let uniqueProjects = {};
       let uniqueContributors = {};
 
-      this.contributionMonths.map(contributionMonth => {
-        if (currentMonth.isSameOrAfter(contributionMonth.date)) {
-          contributionMonth.monthlyOSProjects.map(monthlyOSProject => {
-            uniqueProjects[monthlyOSProject._id] = monthlyOSProject;
-          });
-
-          Object.assign(uniqueContributors,
-            contributionMonth.monthlyContributions.contributorsMap);
-        }
+      this.effectiveMonths.map(contributionMonth => {
+        Object.assign(uniqueContributors,
+          contributionMonth.monthlyContributions.contributorsMap);
       });
 
       Object.keys(uniqueContributors).map(contributorId => {
         let contributor = Object.assign({ }, uniqueContributors[contributorId]);
         contributor.monthlyOSProjects = [];
 
-        Object.keys(uniqueProjects).map(projectId => {
-          let monthlyOSProject = uniqueProjects[projectId];
+        Object.keys(this.monthlyOSProjects).map(projectId => {
+          let monthlyOSProject = this.monthlyOSProjects[projectId];
 
           contributor.monthlyOSProjects.push({
             osProjectRef: monthlyOSProject.osProjectRef,
@@ -123,6 +150,11 @@ export const ViewModel = DefineMap.extend({
 
     return contributorProjectPayouts;
   },
+	/**
+	 * @property {Boolean} hasContributionPayouts
+	 *
+	 * Whethere there are any payouts to display.
+	 */
   get hasContributionPayouts() {
     return Boolean(Object.keys(this.payouts).length);
   },
