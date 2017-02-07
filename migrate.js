2978a97e0dc2e511af92ff9737a8b927b1506f71
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-07 00:50:59 -0500

    set default env to dev for db-migrate

--- migrate.js
+++ migrate.js
@@ -4,10 +4,10 @@
  * ```
  * $ db-migrate up
  * ```
- * 
+ *
  * However, if the migration code calls Service and Model
  * methods directly, then we need to manually close the
- * connection. This file will take care of this for us. Instead 
+ * connection. This file will take care of this for us. Instead
  * of running the `db-migrate` command, run this file instead.
  * Note: This works with multiple migrations.
  *
@@ -19,7 +19,14 @@
 const DBMigrate = require('db-migrate');
 const mongoose = require('mongoose');
 
-const instance = DBMigrate.getInstance(true);
+const options = {};
+
+// Travis runs db-migrate without passing NODE_ENV
+if (!process.env.NODE_ENV){
+	options.env = 'dev';
+}
+
+const instance = DBMigrate.getInstance(true, options);
 
 // same as running `db-migrate up`
 instance.up(() => {
