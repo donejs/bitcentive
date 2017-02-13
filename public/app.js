c7cf43b54be9f40d6c84139aa8bde672d95126d5
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-13 14:18:10 -0500

    formatting

--- public/app.js
+++ public/app.js
@@ -10,60 +10,60 @@ import viewModel from 'can-view-model';
 window.viewModel = viewModel;
 
 var pages = {
-  home: 'public',
-  dashboard: 'private',
-  contributors: 'private',
-  users: 'private'
+	home: 'public',
+	dashboard: 'private',
+	contributors: 'private',
+	users: 'private'
 };
 
 const AppViewModel = DefineMap.extend({
 
-  /**
-   * By default, viewModel attributes will not be serialized into the URL as
-   * route attributes.
-   */
-  '*': {
-    serialize: false
-  },
+	/**
+	 * By default, viewModel attributes will not be serialized into the URL as
+	 * route attributes.
+	 */
+	'*': {
+		serialize: false
+	},
 
-  /**
-   * Use Session.get() to see if there's a valid JWT. If one exists,
-   * a new Session will be created.
-   */
-  session: {
-    get () {
-      return Session.current;
-    }
-  },
+	/**
+	 * Use Session.get() to see if there's a valid JWT. If one exists,
+	 * a new Session will be created.
+	 */
+	session: {
+		get () {
+			return Session.current;
+		}
+	},
 
-  /**
-   * Determines which page-level component is displayed.
-   */
-  page: {
-    serialize: true,
-    get (page) {
-      if (this.session) {
-        if (page === 'home') {
-          page = 'dashboard';
-        }
-      } else {
-        if (pages[page] === 'private') {
-          page = 'home';
-        }
-      }
-      if (!pages[page]) {
-        page = 'four-oh-four';
-      }
-      return page;
-    }
-  },
+	/**
+	 * Determines which page-level component is displayed.
+	 */
+	page: {
+		serialize: true,
+		get (page) {
+			if (this.session) {
+				if (page === 'home') {
+					page = 'dashboard';
+				}
+			} else {
+				if (pages[page] === 'private') {
+					page = 'home';
+				}
+			}
+			if (!pages[page]) {
+				page = 'four-oh-four';
+			}
+			return page;
+		}
+	},
 
-  /**
-   * The `title` attribute is used in index.stache as the HTML title.
-   */
-  title: {
-    value: 'Bitcentive'
-  }
+	/**
+	 * The `title` attribute is used in index.stache as the HTML title.
+	 */
+	title: {
+		value: 'Bitcentive'
+	}
 });
 
 route('{page}', {page: 'home'});
