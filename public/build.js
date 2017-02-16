a5fa17ba8a3091a81201ad6e6d8fa26cbc48ac91
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-16 10:44:48 -0500

    build: minify false, debug false

--- public/build.js
+++ public/build.js
@@ -3,5 +3,7 @@ var stealTools = require("steal-tools");
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
 }, {
-  bundleAssets: true
+  bundleAssets: true,
+	minify: false,
+	debug: true
 });
