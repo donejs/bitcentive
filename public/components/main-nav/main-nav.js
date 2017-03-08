b0e47ef74ac8aa88f8b17c2ae716dbc1ae6d5bd0
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-08 15:50:55 -0500

    docs: components/main-nav

--- public/components/main-nav/main-nav.js
+++ public/components/main-nav/main-nav.js
@@ -1,10 +1,55 @@
+/**
+ * @module {can.Component} bitcentive/components/main-nav <main-nav>
+ * @parent bitcentive.components
+ * @group bitcentive/components/main-nav.prototype 0 prototype
+ * @group bitcentive/components/main-nav.properties 1 properties
+ *
+ * Main navigation of the app
+ *
+ * @signature `<main-nav {(page)}="page" {(session)}="session"></main-nav>`
+ *
+ *   Creates the main navigation
+ *
+ *   @param {String} page The name of the active page
+ *
+ *   @param {bitcentive/models/session} session The session of the app
+ *
+ * @body
+ *
+ * To create a `<main-nav>` element pass the [bitcentive/models/session] and `page`:
+ *
+ * ```
+ * <main-nav
+ *     {(page)}="displayedPage"
+ *     {(session)}="session"></main-nav>
+ * ```
+ *
+ * ## Example
+ *
+ * @demo public/components/main-nav/main-nav.html
+ *
+ */
+
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import view from './main-nav.stache!';
 import Session from 'bitcentive/models/session';
 
 export const ViewModel = DefineMap.extend('MainNav', {
+	/**
+	 * @property {String} bitcentive/components/main-nav.page page
+	 * @parent bitcentive/components/main-nav.properties
+	 *
+	 * The name of the active page.
+	 **/
 	page: 'string',
+
+	/**
+	 * @property {bitcentive/models/session} bitcentive/components/main-nav.session session
+	 * @parent bitcentive/components/main-nav.properties
+	 *
+	 * The session of the application.
+	 **/
 	session: Session
 });
 
