2ad15373d70a631c341e1939ac9c142271446799
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-09 16:57:55 -0500

    docs: session model

--- public/models/session.js
+++ public/models/session.js
@@ -3,6 +3,8 @@
  * @module {can-map} bitcentive/models/session Session
  * @parent bitcentive.clientModels
  *
+ * Represents user's session. Gets populated with data from JWT token.
+ *
  * @group bitcentive/models/session.properties 0 properties
  */
 
@@ -23,7 +25,18 @@ import feathersClient from './feathers-client';
 import User from 'bitcentive/models/user';
 
 export const Session = DefineMap.extend('Session', {
+	/**
+	 * @property {String} bitcentive/models/session.properties.userId userId
+	 * @parent bitcentive/models/session.properties
+	 * Id of the user.
+	 */
   userId: 'any',
+
+	/**
+	 * @property {bitcentive/models/user} bitcentive/models/session.properties.user user
+	 * @parent bitcentive/models/session.properties
+	 * The logged user.
+	 */
   user: {
     Type: User,
     get (lastSetVal, resolve) {
@@ -37,10 +50,21 @@ export const Session = DefineMap.extend('Session', {
   }
 });
 
+/**
+ * @property {can-set/Algebra} bitcentive/models/session.static.algebra algebra
+ * @parent bitcentive/models/session.static
+ * An algebra for the connection. Defines `exp` as id property.
+ */
 Session.algebra =  new set.Algebra(
   set.comparators.id('exp')
 );
 
+/**
+ * @property {can-connect} bitcentive/models/session.static.connection connection
+ * @parent bitcentive/models/session.static
+ *
+ * Uses {can-connect-feathers/Session} behavior and {feathers/client} wrapper around SocketIO to connect to a FeathersJS server.
+ */
 Session.connection = connect([
     feathersSession,
     construct,
